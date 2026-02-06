73a13c6074931d7507165412c9590bb6c4a8aaf5
Ishan Toraskar <156403267+ishanrt119@users.noreply.github.com>
2026-02-06 15:36:33 +0530

    Remove duplicate greeting for Vaibhavee

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
         // This line prints the message to the console
+        System.out.println("Hello, Vaibhavee!");
         System.out.println("Hello, Ishan!");
         System.out.println("Hello, Sharwari!");
         System.out.println("Hello, Soham!");
         System.out.println("Hello, Varad!");
-        System.out.println("Hello, Vaibhavee!");
     }
 }
